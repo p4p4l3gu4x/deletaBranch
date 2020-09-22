f7a07325374524ef96d84026457598b7c4c90456
Hudson Martins <hudson.santos@broadcom.com>
2020-09-22 15:20:19 +0200

    Adjust to python3.

--- branch_delete.py
+++ branch_delete.py
@@ -1,16 +1,17 @@
+#!/usr/bin/python3
+
 from subprocess import check_output
 import sys, getopt
 from distutils.util import strtobool
 from six.moves import input
 
 def listBranches(directory):
-    raw_results = check_output('cd '+str(directory)+'; git branch -r', shell=True)
+    raw_results = check_output('cd '+directory+'; git branch -r', shell=True).decode('utf-8')
     listOfBranches = []
     for b in raw_results.split('\n'):
         if(b != ""):
             branch_name = b.split("/")[1]
             listOfBranches.append(branch_name)
-    
     return listOfBranches
 
 def filter(customList, listOfBranches):
@@ -23,22 +24,24 @@ def filter(customList, listOfBranches):
     return listFiltered
 
 def confirmDelete(branchName):
-    question = "Confirm delete branch '"+ str(branchName) + "' [y/n]: "
+    question = "Confirm delete branch '"+ branchName + "[y/n]': "
     user_input = input(question).lower()
     try:
         return bool(strtobool(user_input))
     except ValueError:
-        print("Please use y/n or yes/no.")
+        print("Please use y/n.")
         confirmDelete(branchName)
 
 def delete_branch(branch):
-    print ("Deleting remote branch '" + str(branch)+ "'")
-    return check_output('git branch -D '+ str(branch), shell=True).strip()
+    print ("Deleting remote branch '" + branch+ "'")
+    return check_output('git branch -D '+ branch, shell=True).strip()
 
 def deleteBranches(listOfBranches):
     for branch in listOfBranches:
         if(confirmDelete(branch)):
             delete_branch(branch)
+        else:
+            print("Branch '", branch, "' not deleted")
 
 def main(argv):
     gitRepoDirectory =  ""
@@ -46,7 +49,7 @@ def main(argv):
     try:
       opts, args = getopt.getopt(argv,"d:b:")
     except getopt.GetoptError:
-      print ('branch_delete -d <gitRepoDirectory>')
+      print ('branch_delete -d <gitRepoDirectory> -b <listBranchesNotDelete>')
       sys.exit(2)
     for opt, arg in opts:
         if opt in ("-d"):
@@ -62,7 +65,8 @@ def main(argv):
     listOfBranches = listBranches(gitRepoDirectory)
     listOfBranches = filter(customList, listOfBranches)
     listOfBranches.sort()
-    print("Found '" + str(len(listOfBranches)) +"' branches to delete")
+    print("Found '", len(listOfBranches), "' branches to delete")
     deleteBranches(listOfBranches)
+
 if __name__ == '__main__':
     main(sys.argv[1:]);
